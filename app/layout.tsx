0171a6a0922561018b907c52ccf5e93e3a804e1d
drili <DriliB@hotmail.com>
2023-12-29 16:56:29 +0100

    infinite scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,24 +9,24 @@ import "./globals.css";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anime Vault",
-  description: "Your favorite anime, all in one place.",
+    title: "Anime Vault",
+    description: "Your favorite anime, all in one place.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={dmSans.className}>
-        <main className="max-w-7xl mx-auto bg-[#0F1117]">
-          <Hero />
-          {children}
-          <Footer />
-        </main>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={dmSans.className}>
+                <main className="max-w-7xl mx-auto bg-[#0F1117]">
+                    <Hero />
+                    {children}
+                    <Footer />
+                </main>
+            </body>
+        </html>
+    );
 }
